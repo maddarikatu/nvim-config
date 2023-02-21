c77353569378b73c76fc0d3ff0e6611baf4221bd
Federico <maddarikatu@tutanota.com>
2023-02-21 13:13:48 +0100

    init

---
+++ lua/plugins.lua
@@ -0,0 +1,55 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+	use { 'wbthomason/packer.nvim' }
+	use { 'ellisonleao/glow.nvim' }
+	use { 'vim-airline/vim-airline' }
+	use { 'vim-airline/vim-airline-themes' }
+	use { 'shaunsingh/nord.nvim' }
+	use { 'm4xshen/autoclose.nvim' }
+	use {
+		'goolord/alpha-nvim',
+		config = function ()
+			require'alpha'.setup(require'alpha.themes.dashboard'.config)
+		end
+	}
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v1.x',
+		requires = {
+			-- LSP Support
+			{'neovim/nvim-lspconfig'},             -- Required
+			{'williamboman/mason.nvim'},           -- Optional
+			{'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+			-- Autocompletion
+			{'hrsh7th/nvim-cmp'},         -- Required
+			{'hrsh7th/cmp-nvim-lsp'},     -- Required
+			{'hrsh7th/cmp-buffer'},       -- Optional
+			{'hrsh7th/cmp-path'},         -- Optional
+			{'saadparwaiz1/cmp_luasnip'}, -- Optional
+			{'hrsh7th/cmp-nvim-lua'},     -- Optional
+
+			-- Snippets
+			{'L3MON4D3/LuaSnip'},             -- Required
+			{'rafamadriz/friendly-snippets'}, -- Optional
+		}
+	}
+	use {
+		'nvim-treesitter/nvim-treesitter',
+		run = function()
+			local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+			ts_update()
+		end,
+	}
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.1',
+		-- or                            , branch = '0.1.x',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+	use 'nvim-tree/nvim-web-devicons'
+	use {'romgrk/barbar.nvim', requires = 'nvim-web-devicons'}
+end)
---
+++ lua/settings.lua
@@ -0,0 +1,15 @@
+vim.opt.termguicolors = true
+vim.opt.cursorline  = true
+vim.opt.number		= true
+vim.opt.mouse		= 'a'
+
+vim.opt.tabstop		= 4
+vim.opt.softtabstop	= 4
+vim.opt.expandtab	= false
+vim.opt.shiftwidth	= 4
+
+vim.opt.foldmethod	= 'syntax'
+vim.opt.foldlevel	= 99
+
+vim.cmd				[[colorscheme nord]]
+vim.g.mapleader		= " "
